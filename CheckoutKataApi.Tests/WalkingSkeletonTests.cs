de822a6b591e4e42dd3c27bdcc32986a38ce6871
Matthew Butt <matthew.butt@7digital.com>
2014-09-24 11:50:34 +0100

    Added first test

---
+++ CheckoutKataApi.Tests/WalkingSkeletonTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace CheckoutKataApi.Tests
+{
+	[TestFixture]
+
+	public class WalkingSkeletonTests
+	{
+		[Test]
+		public void Should_do_something()
+		{
+
+		}
+	}
+}
