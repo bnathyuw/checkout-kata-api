5f2284028f640107a849c2feebfa4837670d46de
Kate Posener <kate.posener@7digital.com>
2014-10-01 11:56:40 +0100

    Added walking skeleton spec

---
+++ CheckoutKataApi.Specs/WalkingSkeleton.feature
@@ -0,0 +1,8 @@
+﻿Feature: Walking Skeleton
+	In order to get the basics in place
+	As a developer of the system
+	I want the API to respond to a simple request
+
+Scenario: Make a simple request
+	When I make a request to the API
+	Then I get a response
---
+++ CheckoutKataApi.Specs/WalkingSkeletonSteps.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace CheckoutKataApi.Specs
+{
+    [Binding]
+    public class WalkingSkeletonSteps
+    {
+        private HttpWebResponse _webResponse;
+
+        [When(@"I make a request to the API")]
+        public void WhenIMakeARequestToTheApi()
+        {
+            var webRequest = WebRequest.Create("http://checkout-kata-api.local/");
+            _webResponse = (HttpWebResponse)webRequest.GetResponse();
+        }
+
+        [Then(@"I get a response")]
+        public void ThenIGetAResponse()
+        {
+            var responseStream = _webResponse.GetResponseStream();
+            var streamReader = new StreamReader(responseStream);
+            var response = streamReader.ReadToEnd();
+
+            Assert.That(_webResponse.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            Assert.That(response, Is.EqualTo("welcome"));
+        }
+    }
+}
