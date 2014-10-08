3051642999aa08615eee34ee2007ccbe98188845
Kate Posener <kate.posener@7digital.com>
2014-10-08 12:13:29 +0100

    Extracted Browser class.

--- CheckoutKataApi.Specs/BasketProcessing.cs
+++ CheckoutKataApi.Specs/BasketProcessing.cs
@@ -8,54 +8,32 @@ namespace CheckoutKataApi.Specs
 {
     public class BasketProcessing
     {
-        private HttpWebResponse _webResponse;
+        private readonly Browser _browser = new Browser();
 
         public Uri CreateBasket(string basketContents)
         {
-            Post(new Uri("http://checkout-kata-api.local/baskets"));
+            _browser.Post(new Uri("http://checkout-kata-api.local/baskets"));
 
-            AssertStatusCodeIs(HttpStatusCode.Created);
+            _browser.AssertStatusCodeIs(HttpStatusCode.Created);
 
-            return new Uri(_webResponse.GetResponseHeader("Location"));
+            return _browser.GetLocationUri();
         }
 
         public void GetBasket(Uri basketUri)
         {
-            Get(basketUri);
+            _browser.Get(basketUri);
 
-            AssertStatusCodeIs(HttpStatusCode.OK);
+            _browser.AssertStatusCodeIs(HttpStatusCode.OK);
         }
 
         public void AssertPriceIsCorrect(int expectedPrice)
         {
-            var responseStream = _webResponse.GetResponseStream();
-            Assert.IsNotNull(responseStream, "responseStream");
-            var streamReader = new StreamReader(responseStream);
-            var body = streamReader.ReadToEnd();
+            var body = _browser.GetResponseBody();
 
             var serializer = new JavaScriptSerializer();
             var basket = serializer.Deserialize<Basket>(body);
 
             Assert.That(basket.Price, Is.EqualTo(expectedPrice));
-        }
-
-        private void Get(Uri requestUri)
-        {
-            var webRequest = WebRequest.Create(requestUri);
-            _webResponse = (HttpWebResponse) webRequest.GetResponse();
-        }
-
-        private void AssertStatusCodeIs(HttpStatusCode httpStatusCode)
-        {
-            Assert.That(_webResponse.StatusCode, Is.EqualTo(httpStatusCode));
-        }
-
-        private void Post(Uri requestUri)
-        {
-            var webRequest = WebRequest.Create(requestUri);
-            webRequest.Method = "POST";
-            webRequest.ContentLength = 0;
-            _webResponse = (HttpWebResponse) webRequest.GetResponse();
         }
     }
 }
---
+++ CheckoutKataApi.Specs/Browser.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using NUnit.Framework;
+
+namespace CheckoutKataApi.Specs
+{
+    public class Browser
+    {
+        private HttpWebResponse _webResponse;
+
+        public void Post(Uri requestUri)
+        {
+            var webRequest = WebRequest.Create(requestUri);
+            webRequest.Method = "POST";
+            webRequest.ContentLength = 0;
+            _webResponse = (HttpWebResponse) webRequest.GetResponse();
+        }
+
+        public void Get(Uri requestUri)
+        {
+            var webRequest = WebRequest.Create(requestUri);
+            _webResponse = (HttpWebResponse) webRequest.GetResponse();
+        }
+
+        public void AssertStatusCodeIs(HttpStatusCode httpStatusCode)
+        {
+            Assert.That(_webResponse.StatusCode, Is.EqualTo(httpStatusCode));
+        }
+
+        public Uri GetLocationUri()
+        {
+            return new Uri(_webResponse.GetResponseHeader("Location"));
+        }
+
+        public string GetResponseBody()
+        {
+            var responseStream = _webResponse.GetResponseStream();
+            Assert.IsNotNull(responseStream, "responseStream");
+            var streamReader = new StreamReader(responseStream);
+            var body = streamReader.ReadToEnd();
+            return body;
+        }
+    }
+}
