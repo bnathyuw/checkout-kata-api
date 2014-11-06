e2d600b90b2e7b20ad2accb0c9e381f231fa33d9
Kate Posener <kate.posener@7digital.com>
2014-11-06 12:47:21 +0000

    Make discount and full price classes

--- CheckoutKataApi.Web/PriceCalculator.cs
+++ CheckoutKataApi.Web/PriceCalculator.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace CheckoutKataApi.Web
 {
@@ -17,19 +18,68 @@ namespace CheckoutKataApi.Web
         }
     }
 
+    public class Discount
+    {
+        private readonly int _groupSize;
+        private readonly int _discountPerGroup;
+        private readonly char _itemCode;
+
+        public Discount(int groupSize, int discountPerGroup, char itemCode)
+        {
+            _groupSize = groupSize;
+            _discountPerGroup = discountPerGroup;
+            _itemCode = itemCode;
+        }
+
+        public int DiscountOnItems(BasketItems basketItems)
+        {
+            return (basketItems.ItemCount(_itemCode)/ _groupSize) * _discountPerGroup;
+        }
+    }
+
+    public class FullPrice
+    {
+        private readonly int _pricePerItem;
+        private readonly char _itemCode;
+
+        public FullPrice(int pricePerItem, char itemCode)
+        {
+            _pricePerItem = pricePerItem;
+            _itemCode = itemCode;
+        }
+
+        public int PriceOfItems(BasketItems basketItems)
+        {
+            return basketItems.ItemCount(_itemCode) * _pricePerItem;
+        }
+    }
+
     public class PriceCalculator
     {
+        private readonly List<FullPrice> _fullPrices;
+        private readonly List<Discount> _discountPrices;
+
+        public PriceCalculator()
+        {
+            _fullPrices = new List<FullPrice>
+                {
+                    new FullPrice(50, 'A'),
+                    new FullPrice(30, 'B'),
+                    new FullPrice(20, 'C'),
+                    new FullPrice(15, 'D')
+                };
+
+            _discountPrices = new List<Discount>
+                {
+                    new Discount(3, 20, 'A'), 
+                    new Discount(2, 15, 'B')
+                };
+        }
+
         public int GetPriceOf(BasketItems basketItems)
         {
-            var itemACount = basketItems.ItemCount('A');
-            var itemBCount = basketItems.ItemCount('B');
-            var itemCCount = basketItems.ItemCount('C');
-            var itemDCount = basketItems.ItemCount('D');
-            
-            return itemACount*50 - (itemACount/3)*20 +
-                   itemBCount*30 - (itemBCount/2)*15 +
-                   itemCCount*20 +
-                   itemDCount*15;
+            return _fullPrices.Sum(price => price.PriceOfItems(basketItems))
+                   - _discountPrices.Sum(price => price.DiscountOnItems(basketItems));
         }
     }
 }
